c363efd03a6f926fd82bbe787d29a5bdec69c756
Leonamin <yism810@gmail.com>
2019-08-09 18:20:32 +0900

    d

---
+++ test.py
@@ -0,0 +1,60 @@
+import RPi.GPIO as GPIO
+from flask import Flask, render_template, request
+import time
+
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+
+pins = {
+   2 : {'name' : 'RPI PWR' , 'state' : GPIO.LOW},
+   3 : {'name' : 'MB PWR'  , 'state' : GPIO.LOW},
+   4 : {'name' : 'Dool'    , 'state' : GPIO.LOW},
+   17 : {'name' : 'UP'     , 'state' : GPIO.LOW},
+   27 : {'name' : 'DOWN'   , 'state' : GPIO.LOW},
+   22 : {'name' : 'STOP'   , 'state' : GPIO.LOW}
+   }
+
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.HIGH)
+
+@app.route("/")
+def main():
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+   templateData = {
+      'pins' : pins
+      }
+   return render_template('main.html', **templateData)
+
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+   changePin = int(changePin)
+   deviceName = pins[changePin]['name']
+   if action == "on":
+      if deviceName == "Door":
+         GPIO.output(changePin, GPIO.HIGH)
+         message = "Turned " + deviceName + " on."
+      else:
+         pass
+   if action == "off":
+      if deviceName == "Door":
+         GPIO.output(changePin, GPIO.LOW)
+         message = "Turned " + deviceName + " off."
+      else:
+         GPIO.output(changePin, GPIO.LOW)
+         time.sleep(0.5)
+         GPIO.output(changePin, GPIO.HIGH)
+
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+
+   templateData = {
+      'pins' : pins
+   }
+
+   return render_template('main.html', **templateData)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
