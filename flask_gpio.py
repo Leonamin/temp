e5c519098750871c477b86679ba17ae043bcf952
Leonamin <yism810@gmail.com>
2019-08-09 16:12:01 +0900

    .

--- flask_gpio.py
+++ flask_gpio.py
@@ -1,67 +1,55 @@
-'''
-
-Adapted excerpt from Getting Started with Raspberry Pi by Matt Richardson
-
-Modified by Rui Santos
-Complete project details: http://randomnerdtutorials.com
-
-'''
-
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
+import time
+
 app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
 
-# Create a dictionary called pins to store the pin number, name, and pin state:
 pins = {
-   2 : {'name' : 'RPI PWR' , 'state' : GPIO.HIGH},
-   3 : {'name' : 'MB PWR'  , 'state' : GPIO.HIGH},
-   4 : {'name' : 'Door'    , 'state' : GPIO.HIGH},
-   17 : {'name' : 'UP'     , 'state' : GPIO.HIGH},
-   27 : {'name' : 'DOWN'   , 'state' : GPIO.HIGH},
-   22 : {'name' : 'STOP'   , 'state' : GPIO.HIGH}
+   2 : {'name' : 'RPI PWR' , 'state' : GPIO.LOW},
+   3 : {'name' : 'MB PWR'  , 'state' : GPIO.LOW},
+   4 : {'name' : 'Door'    , 'state' : GPIO.LOW},
+   17 : {'name' : 'UP'     , 'state' : GPIO.LOW},
+   27 : {'name' : 'DOWN'   , 'state' : GPIO.LOW},
+   22 : {'name' : 'STOP'   , 'state' : GPIO.LOW}
    }
 
-# Set each pin as an output and make it low:
 for pin in pins:
    GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
+   GPIO.output(pin, GPIO.HIGH)
 
 @app.route("/")
 def main():
-   # For each pin, read the pin state and store it in the pins dictionary:
    for pin in pins:
       pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
    templateData = {
       'pins' : pins
       }
-   # Pass the template data into the template main.html and return it to the user
    return render_template('main.html', **templateData)
 
-# The function below is executed when someone requests a URL with the pin number and action in it:
 @app.route("/<changePin>/<action>")
 def action(changePin, action):
-   # Convert the pin from the URL into an integer:
    changePin = int(changePin)
-   # Get the device name for the pin being changed:
    deviceName = pins[changePin]['name']
-   # If the action part of the URL is "on," execute the code indented below:
    if action == "on":
-      # Set the pin high:
-      GPIO.output(changePin, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "Turned " + deviceName + " on."
+      if deviceName == "Door":
+         GPIO.output(changePin, GPIO.HIGH)
+         message = "Turned " + deviceName + " on."
+      else:
+         pass
    if action == "off":
-      GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
+      if deviceName == "Door":
+         GPIO.output(changePin, GPIO.LOW)
+         message = "Turned " + deviceName + " off."
+      else:
+         GPIO.output(changePin, GPIO.LOW)
+         time.sleep(0.5)
+         GPIO.output(changePin, GPIO.HIGH)
 
-   # For each pin, read the pin state and store it in the pins dictionary:
    for pin in pins:
       pins[pin]['state'] = GPIO.input(pin)
 
-   # Along with the pin dictionary, put the message into the template data dictionary:
    templateData = {
       'pins' : pins
    }
